287a1ce33ef81332376bae8d7fdea6b5b1c955f1
Jean Parpaillon <jean.parpaillon@free.fr>
2016-02-15 15:46:39 +0100

    Move extension loading functions to occi_extension

--- src/occi_extension.erl
+++ src/occi_extension.erl
@@ -7,6 +7,8 @@
 
 -module(occi_extension).
 
+-include("occi_log.hrl").
+
 -export([new/1,
 	 name/1,
 	 name/2,
@@ -16,6 +18,9 @@
 	 mixins/1,
 	 add_import/2,
 	 imports/1]).
+
+-export([load_path/1,
+	 load/2]).
 
 
 -type id() :: string().
@@ -89,6 +94,100 @@ add_import(Scheme, E) ->
 imports(E) ->
     maps:get(imports, E).
 
+
+%%
+%% Loading API
+%%
+
+%% @throws enoent | eacces | eisdir | enotdir | enomem
+-spec load_path(file:filename_all()) -> ok.
+load_path(Filename) ->
+    case file:read_file(Filename) of
+	{ok, Bin} ->
+	    load(occi_utils:mimetype(Filename), Bin);
+	{error, Reason} ->
+	    throw(Reason)
+    end.
+
+%% @doc Load a model from an iolist()
+%% Mimetype must be given as {Type :: binary(), SubType :: binary(), []}
+%%
+%% Supported types are:
+%% <ul>
+%%   <li>{<<"application">>, <<"xml">>, []}</li>
+%%   <li>{<<"application">>, <<"occi+xml">>, []}</li>
+%%   <li>{<<"application">>, <<"json">>, []}</li>
+%%   <li>{<<"application">>, <<"occi+json">>, []}</li>
+%% </ul>
+%% @throws {unknown_mimetype, term()}
+%% @throws {parse_error, occi_parser:errors()}
+%% @throws {unknown_extension, occi_extension:id()}
+load(MimeType, Bin) when is_list(Bin); is_binary(Bin) ->
+    case parser(MimeType) of
+	undefined -> 
+	    throw({unknown_mimetype, MimeType});
+	Mod -> 
+	    Ext = Mod:parse_extension(Bin),
+	    load_extension(Ext)
+    end.
+	    
+
+%%%
+%%% Priv
+%%%
+load_extension(E) ->
+    ok = load_imports(imports(E)),
+    ok = load_categories(scheme(E), kinds(E)),
+    ok = load_categories(scheme(E), mixins(E)).
+
+
+%% Check extension exists in the database, throw error if not
+load_imports([]) ->
+    ok;
+
+load_imports([ Scheme | Imports ]) ->
+    ?debug("Import extension: ~s", [Scheme]),
+    case import(Scheme) of
+	{ok, Path} ->
+	    ok = load_path(Path),
+	    load_imports(Imports);
+	{error, Err} ->
+	    throw({import, Err})
+    end.
+
+
+load_categories(_Scheme, []) ->
+    ok;
+
+load_categories(Scheme, [ Cat | Categories ]) ->
+    ?debug("Add category: ~p", [occi_category:id(Cat)]),
+    ok = occi_models:add_category(Scheme, Cat),
+    load_categories(Scheme, Categories).
+
+
+import(Scheme) ->
+    Base = baseurl(),
+    Urls = [{Base ++ "/" ++ http_uri:encode(http_uri:encode(Scheme)) ++ ".xml", http_uri:encode(Scheme) ++ ".xml"}],
+    occi_dl:resource(Scheme, Urls).
+
+
+baseurl() ->
+    case application:get_env(occi, schemas_baseurl, undefined) of
+	undefined ->
+	    throw({undefined_env, schemas_baseurl});
+	{priv_dir, Dir} ->
+	    filename:join([occi_utils:priv_dir(), Dir]);
+	Dir when is_list(Dir) ->
+	    Dir
+    end.
+
+
+parser({<<"application">>, <<"xml">>, []})       -> occi_parser_xml;
+parser({<<"application">>, <<"occi+xml">>, []})  -> occi_parser_xml;
+parser(xml)                                      -> occi_parser_xml;
+parser({<<"application">>, <<"json">>, []})      -> occi_parser_json;
+parser({<<"application">>, <<"occi+json">>, []}) -> occi_parser_json;
+parser(json)                                     -> occi_parser_json.
 
 %%%
 %%% eunit
--- test/load_extension_SUITE.erl
+++ test/load_extension_SUITE.erl
@@ -54,7 +54,7 @@ all() ->
 
 load_extension(Config) -> 
     ExtFile = filename:join([?config(data_dir, Config), "occi-infrastructure.xml"]),
-    occi_models:load_path(ExtFile),
+    occi_extension:load_path(ExtFile),
     ?assertMatch(kind,
 		 occi_category:class(occi_models:category({"http://schemas.ogf.org/occi/infrastructure#", "compute"}))),
     ?assertMatch(kind,
@@ -81,13 +81,13 @@ load_extension(Config) ->
 
 
 load_import(_Config) ->
-    occi_models:load(xml, <<"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-			    "<occi:extension xmlns:occi=\"http://schemas.ogf.org/occi\""
-			    " name=\"Custom\""
-			    " scheme=\"http://example.org/occi#\""
-			    " status=\"stable\" version=\"1\">"
-			    " <occi:import scheme=\"http://schemas.ogf.org/occi/infrastructure#\" />"
-			    "</occi:extension>">>),
+    occi_extension:load(xml, <<"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+			       "<occi:extension xmlns:occi=\"http://schemas.ogf.org/occi\""
+			       " name=\"Custom\""
+			       " scheme=\"http://example.org/occi#\""
+			       " status=\"stable\" version=\"1\">"
+			       " <occi:import scheme=\"http://schemas.ogf.org/occi/infrastructure#\" />"
+			       "</occi:extension>">>),
     ?assertMatch(kind,
 		 occi_category:class(occi_models:category({"http://schemas.ogf.org/occi/infrastructure#", "compute"}))),
     ?assertMatch(kind,
